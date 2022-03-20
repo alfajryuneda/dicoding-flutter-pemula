8e094667d5a1a338bdad158695db3122238570c3
alfajryuneda <alfajryuneda@gmail.com>
2022-03-20 22:08:14 +0700

    add container widget

--- lib/main.dart
+++ lib/main.dart
@@ -39,21 +39,22 @@ class MyApp extends StatelessWidget {
 // }
 
 class FirstScreen extends StatelessWidget {
+  const FirstScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
-          icon: Icon(
+          icon: const Icon(
             Icons.menu,
             color: Colors.white,
           ),
           onPressed: () {},
         ),
-        title: Text('First Screen'),
+        title: const Text('First Screen'),
         actions: <Widget>[
           IconButton(
-            icon: Icon(
+            icon: const Icon(
               Icons.search,
               color: Colors.white,
             ),
@@ -61,11 +62,16 @@ class FirstScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Center(
-        child: Text('Hello world!'),
+      body: Container(
+        child: Text('Hi', style: TextStyle(fontSize: 40)),
+        color: Colors.blueGrey,
+        // width: 200,
+        // height: 100,
+        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.all(10),
       ),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.book),
+        child: const Icon(Icons.book),
         onPressed: () {},
       ),
     );
